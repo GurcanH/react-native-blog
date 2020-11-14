cb1032ea4896ab8dbd19ec58657a1480c6f2407c
Gurcan Hamali <gurcanhamali@gmail.com>
2020-11-14 11:46:33 +1100

    167. Deleting a Post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -11,15 +11,7 @@ const blogReducer = (state, action) => {
       });
     case 'delete_blogpost':
       return state.filter(blogPost => blogPost.id !== action.payload);
-    case 'add_blogpost':
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 99999),
-          title: action.payload.title,
-          content: action.payload.content
-        }
-      ];
+
     default:
       return state;
   }
@@ -33,15 +25,13 @@ const getBlogPosts = dispatch => {
 
 const addBlogPost = dispatch => {
   return async (title, content, callback) => {
-    // await new Promise(resolve => setTimeout(resolve, 2000));
-    // dispatch({ type: 'add_blogpost', payload: { title, content } });
-    // callback();
     await jsonServer.post('/blogposts', { title, content });
     callback();
   };
 };
 const deleteBlogPost = dispatch => {
-  return id => {
+  return async id => {
+    await jsonServer.delete(`/blogposts/${id}`);
     dispatch({ type: 'delete_blogpost', payload: id });
   };
 };
